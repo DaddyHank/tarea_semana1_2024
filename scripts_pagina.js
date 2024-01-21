62fcb3500b454f3f036ec4072d812a3ba3c669c0
DaddyHank <elmago2301@gmail.com>
2024-01-21 15:23:42 -0600

    update

---
+++ scripts_pagina.js
@@ -0,0 +1,7 @@
+function alerta(){
+
+    console.log("hiciste click en el botón de la motivación, guap@");
+
+    alert("sé feliz y que nadie te arrebate esa felicidad. Confía en tí mismo y sobre todo, ten ánimo :D")
+
+}
